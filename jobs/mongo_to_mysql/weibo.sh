66b1521b34d90cfd16021dc0648f09a648f734a2
maxrosy <maxgaogator@sina.com>
2019-07-06 12:51:25 +0800

    update kettle folder structure

--- jobs/mongo_to_mysql/weibo.sh
+++ jobs/mongo_to_mysql/weibo.sh
@@ -3,10 +3,23 @@
 startTime=`date -d "today" "+%Y-%m-%d 00:00:00"`
 endTime=`date -d "today" "+%Y-%m-%d 23:59:59"`
 
-sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weibo_user_post.ktr -param:startTime="$startTime" -param:endTime="$endTime"
+startTime_yesterday=`date -d "yesterday" "+%Y-%m-%d 00:00:00"`
+endTime_yesterday=`date -d "yesterday" "+%Y-%m-%d 23:59:59"`
 
-sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weibo_user_growth.ktr -param:startTime="$startTime" -param:endTime="$endTime"
+sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weibo_jobs/weibo_user_post.ktr -param:startTime="$startTime" -param:endTime="$endTime"
 
-sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weibo_post_media.ktr -param:startTime="$startTime" -param:endTime="$endTime"
+sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weibo_jobs/weibo_user_growth.ktr -param:startTime="$startTime" -param:endTime="$endTime"
 
-sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weibo_post_crawl.ktr -param:startTime="$startTime" -param:endTime="$endTime"
+sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weibo_jobs/weibo_post_media.ktr -param:startTime="$startTime" -param:endTime="$endTime"
+
+sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weibo_jobs/weibo_post_crawl.ktr -param:startTime="$startTime" -param:endTime="$endTime"
+
+sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weibo_jobs/weibo_post_mention.ktr -param:startTime="$startTime" -param:endTime="$endTime"
+
+sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weibo_jobs/weibo_user_info.ktr -param:startTime="$startTime" -param:endTime="$endTime"
+
+sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weibo_jobs/weibo_post_mention_media.ktr -param:startTime="$startTime" -param:endTime="$endTime"
+
+sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weibo_jobs/weibo_search_statuses_limited_media.ktr -param:startTime="$startTime" -param:endTime="$endTime"
+
+sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weibo_jobs/weibo_search_statuses_limited.ktr -param:startTime="$startTime_yesterday" -param:endTime="$endTime_yesterday"
--- jobs/mongo_to_mysql/weixin.sh
+++ jobs/mongo_to_mysql/weixin.sh
@@ -3,8 +3,8 @@
 startTime=`date -d "today" "+%Y-%m-%d 00:00:00"`
 endTime=`date -d "today" "+%Y-%m-%d 23:59:59"`
 
-sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weixin_post.ktr -param:startTime="$startTime" -param:endTime="$endTime"
+sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weixin_jobs/weixin_post.ktr -param:startTime="$startTime" -param:endTime="$endTime"
 
-sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weixin_user_cumulate.ktr -param:startTime="$startTime" -param:endTime="$endTime"
+sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weixin_jobs/weixin_user_cumulate.ktr -param:startTime="$startTime" -param:endTime="$endTime"
 
-sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weixin_upstream_msg.ktr -param:startTime="$startTime" -param:endTime="$endTime"
+sh /home/panther/data-integration/pan.sh -file=/home/panther/SocialSpace/jobs/mongo_to_mysql/weixin_jobs/weixin_upstream_msg.ktr -param:startTime="$startTime" -param:endTime="$endTime"
