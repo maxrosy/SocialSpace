76b72f6df15d31ae01c0e21beca261f3e5adb3f8
maxrosy <maxgaogator@sina.com>
2019-08-16 11:08:45 +0800

    add sub parsers

--- jobs/newrank_to_mongo/newrank_wrapper.py
+++ jobs/newrank_to_mongo/newrank_wrapper.py
@@ -2,10 +2,12 @@ from SocialAPI.SocialAPI.NewRankAPI import NewRankAPI
 import argparse
 
 def main():
+
+    # Add shared arguments
     main_parser = argparse.ArgumentParser(add_help=False)
     main_parser.description = 'Command line interface of test argparser'
-    main_parser.add_argument('--from', required=True, help='start time(included), format: YYYY-mm-dd HH:MM:SS')
-    main_parser.add_argument('--to', required=True, help='end time(not included), format: YYYY-mm-dd HH:MM:SS')
+    main_parser.add_argument('--from', help='start time(included), format: YYYY-mm-dd HH:MM:SS')
+    main_parser.add_argument('--to', help='end time(not included), format: YYYY-mm-dd HH:MM:SS')
     main_parser.add_argument('-ia', '--includeAllWords')
     main_parser.add_argument('-in', '--includeAnyWords')
     main_parser.add_argument('-e', '--excludeWords')
@@ -15,15 +17,20 @@ def main():
                              help="Any optional argument not mentioned above, example: --others=key1||value1||key2||value2")
 
     parser = argparse.ArgumentParser()
-
     subparsers = parser.add_subparsers(help='Choose one application')
 
     weixin_parser = subparsers.add_parser('weixin', help='weixin', parents=[main_parser])
     weixin_subparser = weixin_parser.add_subparsers(help='Choose one function')
-    weixin_search_subparser = weixin_subparser.add_parser('search_content', parents=[main_parser])
 
+    # Add Weixin Search Content function command line
+    weixin_search_subparser = weixin_subparser.add_parser('search_content', parents=[main_parser])
+    weixin_search_subparser.set_defaults(app='weixin',function='search_content')
+
+    # Add Weixin Article Content function command line
     weixin_article_subparser = weixin_subparser.add_parser('article_content', parents=[main_parser])
     weixin_article_subparser.add_argument('account')
+    weixin_article_subparser.set_defaults(app='weixin',function='article_content')
+
     args = parser.parse_args()
     opt = vars(args)
 
@@ -39,10 +46,12 @@ def main():
         del opt[key]
     from_time = opt.pop('from')
     to_time = opt.pop('to')
+
     newrank = NewRankAPI()
-    if opt.get('app') == 'weixin' and opt.get('function') == 'data_combine_search_content':
+    if opt.get('app') == 'weixin' and opt.get('function') == 'search_content':
         newrank.get_weixin_data_combine_search_content(from_time,to_time,**opt)
-    elif opt.get('app') == 'weixin' and opt.get('function') == 'account_article_content':
+    elif opt.get('app') == 'weixin' and opt.get('function') == 'article_content':
+        account = opt.pop('account')
         newrank.get_weixin_account_article_content(account,from_time,to_time,**opt)
 
 if __name__ == "__main__":
